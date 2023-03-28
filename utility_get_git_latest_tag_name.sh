0427eda9d1db2baabc54fdb0ac0ba622ebb93331
mark.hsieh <mark.hsieh@paizaroyal.com.tw>
2023-03-28 06:15:39 +0000

    Update utility_get_git_latest_tag_name.sh

--- utility_get_git_latest_tag_name.sh
+++ utility_get_git_latest_tag_name.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-_TAG=`git describe --abbrev=0 2>/dev/null`
+_TAG=`git describe --tags --abbrev=0 2>/dev/null`
 if [ ! -z "${_TAG}" ]; then
     #echo "the latest tag name is: ${_TAG}"
     echo "${_TAG}"
