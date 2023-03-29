aabe95a205a2fd87fd0e6cd9a17862d127d75acc
mark.hsieh <mark.hsieh@paizaroyal.com.tw>
2023-03-29 02:20:53 +0000

    fix rule

--- utility_namespace_dns.sh
+++ utility_namespace_dns.sh
@@ -3,10 +3,10 @@
 if [ ! -e "/etc/resolv.conf" ]; then
   touch /etc/resolv.conf
   chmod +rw /etc/resolv.conf
-  echo "8.8.8.8 nameserver" | tee -a /etc/resolv.conf >/dev/null
-  echo "168.95.1.1 nameserver" | tee -a /etc/resolv.conf >/dev/null
-  echo "61.31.1.1 nameserver" | tee -a /etc/resolv.conf >/dev/null
-  echo "208.67.222.222 nameserver" | tee -a /etc/resolv.conf >/dev/null
+  echo "nameserver 8.8.8.8 " | tee -a /etc/resolv.conf >/dev/null
+  echo "nameserver 168.95.1.1 " | tee -a /etc/resolv.conf >/dev/null
+  echo "nameserver 61.31.1.1 " | tee -a /etc/resolv.conf >/dev/null
+  echo "nameserver 208.67.222.222 " | tee -a /etc/resolv.conf >/dev/null
 else
   cat /etc/resolv.conf
 fi
