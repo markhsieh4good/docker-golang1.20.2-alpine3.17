0cb50cc97f29a8a5b84345773d8ecd95b19033fc
mark.hsieh <mark.hsieh@paizaroyal.com.tw>
2023-03-29 02:16:57 +0000

    use docker default setting for resolv.conf.

--- utility_namespace_dns.sh
+++ utility_namespace_dns.sh
@@ -2,11 +2,11 @@
 
 if [ ! -e "/etc/resolv.conf" ]; then
   touch /etc/resolv.conf
-fi
-
-if [ -e "/etc/resolv.conf" ]; then
+  chmod +rw /etc/resolv.conf
   echo "8.8.8.8 nameserver" | tee -a /etc/resolv.conf >/dev/null
   echo "168.95.1.1 nameserver" | tee -a /etc/resolv.conf >/dev/null
   echo "61.31.1.1 nameserver" | tee -a /etc/resolv.conf >/dev/null
   echo "208.67.222.222 nameserver" | tee -a /etc/resolv.conf >/dev/null
+else
+  cat /etc/resolv.conf
 fi
